b1bb2a414711e44f7972ae4d812b6c72f750cc82
Guilherme Rodrigues <guilherme.r1307@gmail.com>
2019-05-23 14:50:56 -0300

    commit save

--- dietmaker/dietmaker/accounts/views.py
+++ dietmaker/dietmaker/accounts/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 
+
+def dashboard(request):
+	template_name = 'accounts/dashboard.html'
+	return render(request, template_name)
+
 def register(request):
 	template_name = 'accounts/signup.html'
 	if request.method == 'POST':
