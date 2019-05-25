0252d18d061cac4d1a5d9a2f0c5e6abc5693d204
Mad <guilherme.r1307@gmail.com>
2019-05-25 15:20:21 -0300

    commiterror2

--- dietmaker/dietmaker/accounts/forms.py
+++ dietmaker/dietmaker/accounts/forms.py
@@ -3,11 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-	CHOICES = ('ALUNO', 'PROFESSOR')
+	CHOICES = [(('ALUNO'), ('PROFESSOR'))]
 	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 	user_type = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple())
 
 	class Meta:
 		model = User
+		exclude = ()
 		fiels = ('username', 'email', 'user_type', 'password1', 'password2')
 
